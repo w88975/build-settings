8dca4dc279f566db9b8e8f596e6d3e56982c3050
kamisama <1457434343@qq.com>
2015-03-19 15:20:33 +0800

    修复windows下不能打开路径的BUG

--- editor/elements/build-settings.js
+++ editor/elements/build-settings.js
@@ -194,7 +194,7 @@ Polymer({
     },
 
     showInFinder: function () {
-        Shell.showItemInFolder(this.settings.buildPath.replace(/\\/g,"/"));
+        Shell.showItemInFolder(Path.normalize(this.settings.buildPath));
         Shell.beep();
     },
 
